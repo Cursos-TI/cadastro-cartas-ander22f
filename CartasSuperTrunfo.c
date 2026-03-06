e0a3084a47fcd12aa0635f75bb102111d228a071
ander22f <ander22f@gmail.com>
2026-03-06 16:20:32 +0000

    Código nivel novato

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -6,10 +6,43 @@
 
 int main() {
   // Área para definição das variáveis para armazenar as propriedades das cidades
+  char estado[25], codigo[25], cidade[25];
+  int populacao;
+  int turismo;
+  float area;
+  float pib;
 
   // Área para entrada de dados
+  printf("Digite o estado:\n");
+  scanf("%s", &estado);
+
+  printf("Digite o código da carta: (Estado + numero da carta)\n");
+  scanf("%s", &codigo);
+
+  printf("Digite o nome da cidade:\n");
+  scanf("%s", &cidade);
+
+  printf("Digite a população:\n");
+  scanf("%d", &populacao);
+
+  printf("Digite a quantidade de pontos turisticos:\n");
+  scanf("%d", &turismo);
+
+  printf("Digite a área (em km²):\n");
+  scanf("%f", &area);
+
+  printf("Digite o PIB (em bilhões de reais):\n");
+  scanf("%f", &pib);
 
   // Área para exibição dos dados da cidade
+  printf("Estado: %s\n", estado);
+  printf("Código: %s\n", codigo);
+  printf("Cidade: %s\n", cidade);
+  printf("População: %d\n", populacao);
+  printf("Pontos Turísticos: %d\n", turismo);
+  printf("Área: %.2f km²\n", area);
+  printf("PIB: %.2f bilhões de reais\n", pib);
+
 
 return 0;
 } 
