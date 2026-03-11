8148c2c3307bc5db79b3150a93c0c6acb09f31fc
ander22f <ander22f@gmail.com>
2026-03-11 00:30:18 +0000

    Acressentei o calculo de Densidade populacional e o PIB per capita finalizando o nivel aventureiro.

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -10,12 +10,16 @@
   int turismo1;
   float area1;
   float pib1;
+  float pibpc01;
+  float densidadep01;
 
   char estado2[25], codigo2[25], cidade2[25]; // Variáveis para armazenar os dados da carta 02
   int populacao2;
   int turismo2;
   float area2;
   float pib2;
+  float pibpc02;
+  float densidadep02; 
 
   void entradadadoscarta01(){ // Área para entrada de dados da carta 01
     printf("** CARTA 01 **\n");
@@ -65,6 +69,22 @@
     scanf("%f", &pib2);
   }
 
+ void calculopibpc01(){ // Área para cálculo do PIB per capita da carta 01
+    pibpc01 = (float) pib1*1000000000 / populacao1;
+ }
+
+  void calculopibpc02(){ // Área para cálculo do PIB per capita da carta 02
+    pibpc02 = (float) pib2*1000000000 / populacao2;
+ }
+
+void calculodensidadep01(){ // Área para cálculo da densidade populacional da carta 01
+    densidadep01 = (float) populacao1 / area1;
+}
+
+void calculodensidadep02(){ // Área para cálculo da densidade populacional da carta 02
+    densidadep02 = (float) populacao2 / area2;
+}
+
    void exibirdadoscarta01(){ // Área para exibição dos dados da carta 01
     printf("** CARTA 01 **\n");
     printf("Estado: %s\n", estado1);
@@ -73,7 +93,9 @@
     printf("População: %d\n", populacao1);
     printf("Pontos Turísticos: %d\n", turismo1);
     printf("Área: %.2f km²\n", area1);
-    printf("PIB: %.2f bilhões de reais\n\n", pib1);
+    printf("PIB: %.2f bilhões de reais\n", pib1);
+    printf("Densidade Populacional: %.2f hab/km²\n", densidadep01);
+    printf("PIB per capita: %.2f reais\n\n", pibpc01);
    }
 
     void exibirdadoscarta02(){ // Área para exibição dos dados da carta 02
@@ -84,13 +106,23 @@
     printf("População: %d\n", populacao2);
     printf("Pontos Turísticos: %d\n", turismo2);
     printf("Área: %.2f km²\n", area2);
-    printf("PIB: %.2f bilhões de reais\n\n", pib2);
+    printf("PIB: %.2f bilhões de reais\n", pib2);
+    printf("Densidade Populacional: %.2f hab/km²\n", densidadep02);
+    printf("PIB per capita: %.2f reais\n\n", pibpc02);
    }
 
 int main() {
  
 entradadadoscarta01(); // Função para entrada de dados da carta 01
+calculopibpc01(); // Função para cálculo do PIB per capita da carta 01
+
 entradadadoscarta02(); // Função para entrada de dados da carta 02
+
+
+calculopibpc02(); // Função para cálculo do PIB per capita da carta 02
+
+calculodensidadep01(); // Função para cálculo da densidade populacional da carta 01
+calculodensidadep02(); // Função para cálculo da densidade populacional da carta 02
 
 exibirdadoscarta01(); // Função para exibição dos dados da carta 01
 exibirdadoscarta02(); // Função para exibição dos dados da carta 02
